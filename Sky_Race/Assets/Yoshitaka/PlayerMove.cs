5841ad8657a76b259510302e798a3d59778bdbad
Yoshi696 <qiandaijicheng@gmail.com>
2020-09-15 16:00:27 +0900

    主人公動き、第一段階です。

---
+++ Sky_Race/Assets/Yoshitaka/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 15f;
+    public float zen = 0.05f;
+    //private Vector3 playerpos;
+
+    //private void Start()
+    //{
+    //    playerpos = transform.position;
+    //}
+
+    private void Update()
+    {
+        Vector3 pos = this.gameObject.transform.position;
+        this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z + zen);
+    }
+
+    private void FixedUpdate()
+    {
+        var velox = speed * Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody>().velocity = new Vector3(velox, 0f, 0f);
+        //transform.position = new Vector3(playerpos.x , playerpos.y, playerpos.z + 10.0f);
+    }
+}
