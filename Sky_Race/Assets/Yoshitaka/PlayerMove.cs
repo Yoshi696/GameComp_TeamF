1bfff8b0a2c099ddd9939f594d4a8db230832fdd
Yoshi696 <qiandaijicheng@gmail.com>
2020-09-25 11:34:09 +0900

    コントローラ実装

--- Sky_Race/Assets/Yoshitaka/PlayerMove.cs
+++ Sky_Race/Assets/Yoshitaka/PlayerMove.cs
@@ -4,31 +4,61 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //public Rigidbody rb;
     public float speed = 15f;
     public float zen = 0.05f;
     //private Vector3 playerpos;
 
     //private void Start()
     //{
-    //    playerpos = transform.position;
+    ////    playerpos = transform.position;
+    //rb = GetComponent<Rigidbody>();
+
     //}
 
     private void Update()
     {
         Vector3 pos = this.gameObject.transform.position;
         this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z + zen);
+        float den = pos.y;
+
+        //キーボード
+        float velox = speed * Input.GetAxisRaw("Horizontal");
+        float nox = speed * Input.GetAxisRaw("Vertical");
+        float gen = Input.GetAxisRaw("Vertical");
+        //if (pos.y < den)
+        //{
+        //rb.AddForce(velox, nox, 0f);
+        if (gen < 0) {
+            GetComponent<Rigidbody>().velocity = new Vector3(velox, nox, 0f);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(velox, 0f, 0f);
+        }
     }
 
     private void FixedUpdate()
     {
-        var velox = speed * Input.GetAxisRaw("Horizontal");
-        GetComponent<Rigidbody>().velocity = new Vector3(velox, 0f, 0f);
-        //transform.position = new Vector3(playerpos.x , playerpos.y, playerpos.z + 10.0f);
-        if (Input.GetKey("down"))
-        {
-            var nox = speed * Input.GetAxisRaw("Vertical");
-            GetComponent<Rigidbody>().velocity = new Vector3(0f, nox, 0f);
-            //GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-        }
+        //下にだけ反応するようにする
+
+
+        //コントローラ
+        //var luck = speed * Input.GetAxisRaw("Controler1RL");
+        //GetComponent<Rigidbody>().velocity = new Vector3(luck, 0f, 0f);
+
+        //Vector3 tmp = GameObject.Find("Player").transform.position;
+        //float y = tmp.y;
+
+
+        //if (/*Input.GetKey("down")*/ /*||*/ Input.GetButton("Vertical"))
+        //{
+
+        //velox = speed * Input.GetAxisRaw("Vertical");
+        //GetComponent<Rigidbody>().velocity = new Vector3(0f, velox, 0f);
+        //velox = speed * Input.GetAxisRaw("Controler1D");
+        //GetComponent<Rigidbody>().velocity = new Vector3(0f, velox, 0f);
+        //GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        //}
     }
 }
